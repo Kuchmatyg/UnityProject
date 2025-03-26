a8bb055d79f55cb12b646a13829e0b298f493d6c
KovDan <koval_dan2004@mail.ru>
2025-03-26 13:08:29 +0700

    Часть 1 сделана DestroyModule

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
+    [Tooltip("Задержка между удалениями отдельных частей")]
+    [SerializeField, Min(0.01f)]   
     private float destroyDelay;
+
+    [Tooltip("Минимальное количество дочерних объектов, которое должно остаться после удаления")]
+    [SerializeField, Min(0)]
     private int minimalDestroyingObjectsCount;
 
     private Transform myTransform;
@@ -12,10 +18,17 @@ public class DestroyModule : MonoBehaviour
     private void Awake()
     {
         myTransform = transform;
+        OnValidate();
     }
 
+    [ContextMenu("Начать удаление объектов")]
     public void ActivateModule()
     {
+        if (myTransform.childCount <= minimalDestroyingObjectsCount)
+        {
+            Debug.LogWarning("Недостаточно дочерних объектов для активации модуля!");
+            return;
+        }
         StartCoroutine(DestroyRandomChildObjectCoroutine());
     }
 
@@ -28,5 +41,10 @@ public class DestroyModule : MonoBehaviour
             yield return new WaitForSeconds(destroyDelay);
         }
         Destroy(gameObject, Time.deltaTime);
+    }
+    private void OnValidate()
+    {
+        if (destroyDelay > 5f) destroyDelay = 5f;
+        if (minimalDestroyingObjectsCount > 216) minimalDestroyingObjectsCount = 216;
     }
 }
